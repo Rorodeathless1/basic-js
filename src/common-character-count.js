5d7d893b65f686ac61deb36bd97f0e44d46b34f5
Rorodeathless1 <romochka-suvorov-2016@mail.ru>
2025-10-22 12:05:32 +0300

    feat: add 2 task

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,9 +12,30 @@ const { NotImplementedError } = require('../lib');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 
-function getCommonCharacterCount(/* s1, s2 */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function createCharMap(s) {
+  const map = {};
+  for (const char of s) {
+    map[char] = (map[char] || 0) + 1;
+  }
+  return map;
+}
+
+function getCommonCharacterCount(s1, s2) {
+  const map1 = createCharMap(s1);
+  const map2 = createCharMap(s2);
+
+  let commonCount = 0;
+
+  for (const char in map1) {
+    if (map2[char]) {
+      const countInS1 = map1[char];
+      const countInS2 = map2[char];
+      
+      commonCount += Math.min(countInS1, countInS2);
+    }
+  }
+
+  return commonCount;
 }
 
 module.exports = {
